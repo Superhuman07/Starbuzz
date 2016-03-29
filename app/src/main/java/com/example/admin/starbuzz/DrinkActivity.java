581ef1db29ac488305c76e0555429f1f31c5c888
Superhuman07 <Superhuman07>
2016-03-30 00:33:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/admin/starbuzz/DrinkActivity.java
@@ -0,0 +1,27 @@
+package com.example.admin.starbuzz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DrinkActivity extends AppCompatActivity {
+public final static String EXTRA_DRINKNO = "drinkNo";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+        int drinkNo = (Integer) getIntent().getExtras().get(EXTRA_DRINKNO);
+        Drink drink = Drink.drinks[drinkNo];
+
+        ImageView imageView = (ImageView) findViewById(R.id.photo);
+        imageView.setImageResource(drink.getImageResourceId());
+        imageView.setContentDescription(drink.getDescription());
+
+        TextView name = (TextView) findViewById(R.id.name);
+        name.setText(drink.getName());
+
+        TextView description = (TextView) findViewById(R.id.description);
+        description.setText(drink.getDescription());
+    }
+}
