9429dcfe0827c9ed4e32ca5f84e96f2724fad1cf
Superhuman07 <bhatiarahul51@gmail.com>
2016-04-12 23:03:24 +0530

    Fifth Commit

--- app/src/main/java/com/example/admin/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/example/admin/starbuzz/DrinkCategoryActivity.java
@@ -2,21 +2,55 @@ package com.example.admin.starbuzz;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class DrinkCategoryActivity extends ListActivity {
-
+private SQLiteDatabase db;
+    private Cursor cursor;
+    ListView listDrinks = getListView();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<Drink>(
+        /*ArrayAdapter<Drink> listAdapter = new ArrayAdapter<Drink>(
                 this, android.R.layout.simple_list_item_1, Drink.drinks
         );
         ListView listView = getListView();
-        listView.setAdapter(listAdapter);
+        listView.setAdapter(listAdapter);*/
+        try{
+            SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+
+
+            cursor = db.query("DRINK", new String[]{"_id", "NAME"}, null, null, null, null, null);
+
+            // Simple cursor adapter
+            CursorAdapter listAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1
+            ,cursor, new String[] {"NAME"}, new int[] {android.R.id.text1}, 0);
+
+            listDrinks.setAdapter(listAdapter);
+        }
+        catch (SQLiteException e)
+        {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        cursor.close();
+        db.close();
     }
 
     @Override
